d3d90d201b253586be8809bc7a68ecbaef0d513f
penicillin0 <ayahitonakamura@gmail.com>
2025-05-19 23:23:13 +0900

    feat: useOrientationInfoフックに状態管理を追加

--- hooks/useOrientationInfo.tsx
+++ hooks/useOrientationInfo.tsx
@@ -1,7 +1,14 @@
 import * as ScreenOrientation from "expo-screen-orientation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const useOrientationInfo = () => {
+  const [orientation, setOrientation] =
+    useState<ScreenOrientation.Orientation | null>(null);
+  const [orientationLock, setOrientationLock] =
+    useState<ScreenOrientation.OrientationLock | null>(null);
+  const [platformOrientation, setPlatformOrientation] =
+    useState<ScreenOrientation.PlatformOrientationInfo | null>(null);
+
   useEffect(() => {
     (async () => {
       const orientation = await ScreenOrientation.getOrientationAsync();
@@ -14,6 +21,10 @@ export const useOrientationInfo = () => {
         orientationLock,
         platformOrientation,
       });
+
+      setOrientation(orientation);
+      setOrientationLock(orientationLock);
+      setPlatformOrientation(platformOrientation);
     })();
 
     ScreenOrientation.addOrientationChangeListener((event) => {
@@ -24,4 +35,10 @@ export const useOrientationInfo = () => {
       ScreenOrientation.removeOrientationChangeListeners();
     };
   }, []);
+
+  return {
+    orientation,
+    orientationLock,
+    platformOrientation,
+  };
 };
